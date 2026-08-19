0889b2b1ac15e5e815935469636b8351eeb6e5f6
DESKTOP-0A3K9BM\DELL <lethannhdat07@gmail.com>
2026-08-19 15:21:20 +0700

    up

---
+++ MvcBeginner/Controllers/Product.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcBeginner.Controllers
+{
+    public class Product : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcBeginner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcBeginner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
