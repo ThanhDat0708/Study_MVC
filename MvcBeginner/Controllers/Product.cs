b824408f1fdc2756d5c3238b04bbb6a6de103b17
DESKTOP-0A3K9BM\DELL <lethannhdat07@gmail.com>
2026-08-27 22:59:18 +0700

    feat:learn ASP.NET Core MVC basics

--- MvcBeginner/Controllers/Product.cs
+++ MvcBeginner/Controllers/Product.cs
@@ -4,17 +4,21 @@ using MvcBeginner.Models;
 using System.Threading.Tasks;
 namespace MvcBeginner.Controllers
 {
-    public class Product : Controller
+    public class ProductController : Controller
     {
         private readonly AppDataContext _db;
-        public Product(AppDataContext db)
+        public ProductController(AppDataContext db)
         {
             _db = db;
         }
         
         public async Task<IActionResult> Index()
         {
-            var products = await _db.Products.ToListAsync();
+            //var products = await _db.Products.ToListAsync();
+            var products = await _db.Products
+                .Include(x => x.Category)
+                .Include(x=>x.Supplier)
+                .ToListAsync();
 
             return View(model: products);
         }
