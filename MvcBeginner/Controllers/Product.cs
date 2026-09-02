a56dfbdd5567a33d32ed5e21d88df2448d8431d4
DESKTOP-0A3K9BM\DELL <lethannhdat07@gmail.com>
2026-09-02 22:10:19 +0700

    feat: implement product CRUD operations

--- MvcBeginner/Controllers/Product.cs
+++ MvcBeginner/Controllers/Product.cs
@@ -24,7 +24,7 @@ namespace MvcBeginner.Controllers
                    Price = x.Price,
                    Stock = x.Stock,
                    CategoryName = x.Category.Name,
-                     SupplierName = x.Supplier.Name
+                   SupplierName = x.Supplier.Name
                })
             .ToListAsync();
             return View(products);
@@ -38,20 +38,82 @@ namespace MvcBeginner.Controllers
                 Name = model.Name,
                 Price = model.Price,
                 Stock = model.Stock,
-                 CategoryId = model.CategoryId,
-                 SupplierId = model.SupplierId
+                CategoryId = model.CategoryId,
+                SupplierId = model.SupplierId
             };
             _db.Products.Add(products);
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
-           
+
         [HttpGet]
         public async Task<IActionResult> Create()
         {
-            ViewBag.Categories =await _db.Categories.ToListAsync();
-            ViewBag.Suppliers =await _db.Suppliers.ToListAsync();
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+            ViewBag.Suppliers = await _db.Suppliers.ToListAsync();
             return View();
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var product = await _db.Products.FindAsync(Id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            var model = new ProductEditViewModels
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                Stock = product.Stock,
+                CategoryId = product.CategoryId,
+                SupplierId = product.SupplierId
+            };
+            ViewBag.Categories = await _db.Categories.ToListAsync();
+            ViewBag.Suppliers = await _db.Suppliers.ToListAsync();
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductEditViewModels model)
+        {
+            var product = await _db.Products.FindAsync(model.Id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            product.Name = model.Name;
+            product.Price = model.Price;
+            product.Stock = model.Stock;
+            product.CategoryId = model.CategoryId;
+            product.SupplierId = model.SupplierId;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _db.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            var product = await _db.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            _db.Products.Remove(product);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
     }
+    
 }
